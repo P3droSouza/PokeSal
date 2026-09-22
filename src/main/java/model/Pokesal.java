06e7a7983af90566bfb51093af0dda8ffa59be85
Pedro Souza <pedrohenrique150605@gmail.com>
2026-09-22 10:13:30 -0300

    Aplicando UPPER_CASE na Constante VELOCIDADEMINIMA

--- src/main/java/model/Pokesal.java
+++ src/main/java/model/Pokesal.java
@@ -4,7 +4,7 @@ import enums.StatusDoEfeito;
 import exception.AtaqueBloqueadoException;
 
 public class Pokesal {
-    public static final int VElOCIDADADEMINIMA = 1;
+    public static final int VElOCIDADADE_MINIMA = 1;
 
     private final String nome;
     public final int defesa;
@@ -58,7 +58,7 @@ public class Pokesal {
             this.numeroDeAtaques++;
         }
     public void reduzirSpd(int quantidade) {
-        this.velocidade = Math.max(VElOCIDADADEMINIMA, this.velocidade - quantidade);
+        this.velocidade = Math.max(VElOCIDADADE_MINIMA, this.velocidade - quantidade);
     }
 
     public String getNome() {
